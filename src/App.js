6943a5eb6280ad945d35df22bf48605c27783fe8
Rory <rory88@gmail.com>
2016-12-02 23:41:46 +0000

    Add test state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        'foo',
+        'bar',
+        'baz'
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
